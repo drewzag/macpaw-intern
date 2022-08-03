a644ed23a1f71a51698eb281b7ceca0568a2dc66
drewzag <megafurfur@gmail.com>
2022-08-03 20:52:58 +0300

    fix: refactor display loading

--- src/pages/GalleryPage.tsx
+++ src/pages/GalleryPage.tsx
@@ -4,11 +4,10 @@ import { useGetCatsQuery } from '../data/cat-api/cat.api'
 export const GalleryPage = () => {
   const { data, isLoading } = useGetCatsQuery({ limit: '10', page: '' })
 
-  if (isLoading) return <div>Loading...</div>
   return (
     <>
       <div>GalleryPage</div>
-      {data && <ImagesGrid data={data} />}
+      {isLoading ? <div>Loading...</div> : data && <ImagesGrid data={data} />}
     </>
   )
 }
