8991d71bccd415487eadb6a9e0ad2c7f318bcdb1
drewzag <megafurfur@gmail.com>
2022-08-09 21:16:50 +0300

    styled buttons with image

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,24 +1,51 @@
 import styled from 'styled-components'
-import {ReactComponent as VoteTable} from '../assets/vote-table.svg'
+import VoteTable from '../assets/vote-table.png'
+import PetBreeds from '../assets/pet-breeds.png'
+import ImageSearch from '../assets/images-search.png'
 
-const NavButton = styled.button<{backgroundColor: string, borderColor: string}>`
+const NavBlock = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const NavImageButton = styled.button<{backgroundColor: string, borderColor: string}>`
 position: relative;
 box-sizing: border-box;
 width: 140px;
 height: 200px;
 border-radius: 20px;
+overflow: hidden;
 border: 4px solid ${props => props.borderColor};
 background-color: ${props => props.backgroundColor};
+img {
+  position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
 `
 
 export const Navigation = () => {
 
   return <>
   <div>Navigation</div>
+  <NavBlock>
   <div className='vote'>
-  <NavButton backgroundColor='#B4B7FF' borderColor='rgba(255, 255, 255, 0.6)'>
-  <VoteTable />
-  </NavButton>
+  <NavImageButton backgroundColor='#B4B7FF' borderColor='rgba(255, 255, 255, 0.6)'>
+  <img src={VoteTable} />
+  </NavImageButton>
   </div>
+  <div className='pet-breed'>
+  <NavImageButton backgroundColor='#97EAB9' borderColor='rgba(255, 255, 255, 0.6)'>
+  <img src={PetBreeds} />
+  </NavImageButton>
+  </div>
+  <div className='image-search'>
+  <NavImageButton backgroundColor='#FFD280' borderColor='rgba(255, 255, 255, 0.6)'>
+  <img src={ImageSearch} />
+  </NavImageButton>
+  </div>
+  </NavBlock>
   </>
 }
