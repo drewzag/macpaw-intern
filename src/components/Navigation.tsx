37ce00d1bf39d7756f7c256eed83e5f0e0433137
drewzag <megafurfur@gmail.com>
2022-08-09 21:55:55 +0300

    feat: add routing to nav buttons

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,22 +2,31 @@ import styled from 'styled-components'
 import VoteTable from '../assets/vote-table.png'
 import PetBreeds from '../assets/pet-breeds.png'
 import ImageSearch from '../assets/images-search.png'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBlock = styled.div`
+/* delete background color */
+background-color: #F8F8F7;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  width: 500px;
 `
 
-const NavImageButton = styled.button<{backgroundColor: string, borderColor: string}>`
+const NavImageButton = styled.button<{backgroundColor: string, active: boolean}>`
 position: relative;
 box-sizing: border-box;
 width: 140px;
 height: 200px;
 border-radius: 20px;
 overflow: hidden;
-border: 4px solid ${props => props.borderColor};
+border: 4px solid ${props => props.active ? '#FBE0DC' : 'rgba(255, 255, 255, 0.6)'};
 background-color: ${props => props.backgroundColor};
+&:hover {
+  border-color: #fff;
+  cursor: pointer;
+}
 img {
   position: absolute;
     top: 50%;
@@ -27,25 +36,25 @@ img {
 `
 
 export const Navigation = () => {
-
+const [act, setAct] = useState(false)
   return <>
   <div>Navigation</div>
   <NavBlock>
-  <div className='vote'>
-  <NavImageButton backgroundColor='#B4B7FF' borderColor='rgba(255, 255, 255, 0.6)'>
+  <Link to='/voting'>
+  <NavImageButton backgroundColor='#B4B7FF' active={act} onClick={() => setAct(!act)}>
   <img src={VoteTable} />
   </NavImageButton>
-  </div>
-  <div className='pet-breed'>
-  <NavImageButton backgroundColor='#97EAB9' borderColor='rgba(255, 255, 255, 0.6)'>
+  </Link>
+  <Link to='/breeds'>
+  <NavImageButton backgroundColor='#97EAB9' active={false}>
   <img src={PetBreeds} />
   </NavImageButton>
-  </div>
-  <div className='image-search'>
-  <NavImageButton backgroundColor='#FFD280' borderColor='rgba(255, 255, 255, 0.6)'>
+  </Link>
+  <Link to='/gallery'>
+  <NavImageButton backgroundColor='#FFD280' active={false}>
   <img src={ImageSearch} />
   </NavImageButton>
-  </div>
+  </Link>
   </NavBlock>
   </>
 }
