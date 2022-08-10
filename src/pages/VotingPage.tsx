baf49627f6c761d730ffb96270f41fa83745d210
drewzag <megafurfur@gmail.com>
2022-08-10 14:30:57 +0300

    feat: add style for small buttons

--- src/pages/VotingPage.tsx
+++ src/pages/VotingPage.tsx
@@ -1,9 +1,20 @@
+import styled from 'styled-components'
 import { ImagesGrid } from '../components/ImagesGrid'
 import {
   useGetCatsQuery,
   useMakeFavouriteMutation,
   useMakeVoteMutation,
 } from '../data/cat-api/cat.api'
+import { ReactComponent as Like } from '../assets/like.svg'
+import { ReactComponent as Dislike } from '../assets/dislike-30.svg'
+import { ReactComponent as Favourites } from '../assets/fav-30.svg'
+
+const SmallButton = styled.button`
+  width: 60px;
+  height: 60px;
+  border-radius: 20px;
+  border: none;
+`
 
 export const VotingPage = () => {
   const { data, isLoading, isError } = useGetCatsQuery({ limit: '1' })
@@ -13,6 +24,15 @@ export const VotingPage = () => {
   return (
     <>
       <div>VotingPage</div>
+      <SmallButton>
+        <Like fill='red' />
+      </SmallButton>
+      <SmallButton>
+        <Dislike />
+      </SmallButton>
+      <SmallButton>
+        <Favourites />
+      </SmallButton>
       {isLoading ? (
         <div>Loading...</div>
       ) : (
